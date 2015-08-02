0d075152b709556a9a854f4abe86ae2f47e32eed
Tim Park <timfpark@gmail.com>
2015-08-01 20:51:30 -0700

    add cross origin

--- server.js
+++ server.js
@@ -8,6 +8,19 @@ var mongoose = require('mongoose');
 //console.log('connecting to MongoDB: ' + process.env.MONGODB_CONNECTION_STRING);
 //mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
 
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'OPTIONS,GET,POST,PUT,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    } else {
+        next();
+    }
+});
 app.use(express.logger());
 app.use(express.bodyParser());
 
